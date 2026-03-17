3732df961174d25af6f771058da4d55cab281833
auiechko <auechko@gmail.com>
2026-03-17 11:51:19 +0200

    added is_admin field for user

--- src/main/java/com/example/telos/model/User.java
+++ src/main/java/com/example/telos/model/User.java
@@ -32,6 +32,9 @@ public class User {
     @Column(name="created_at", nullable = false)
     private LocalDateTime createdAt;
 
+    @Column(name = "is_admin", nullable = false)
+    private Boolean isAdmin;
+
     @PrePersist
     private void prePersist() {
         this.createdAt = LocalDateTime.now();
