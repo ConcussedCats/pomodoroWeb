5ed9d92eaf8b73b5188ca5e4ea0e276e1e8a7c5e
auiechko <auechko@gmail.com>
2026-03-19 19:48:06 +0200

    Added test data for todos table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -55,5 +55,6 @@ CREATE TABLE todos (
     CONSTRAINT fk_todos_user FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );
 
+CREATE INDEX idx_user_time_settings ON user_time_settings(user_id);
 CREATE INDEX idx_notes_user_id ON notes(user_id);
 CREATE INDEX idx_todos_user_id ON todos(user_id);
