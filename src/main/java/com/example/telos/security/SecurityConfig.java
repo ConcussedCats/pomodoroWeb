e76bc8c5e8adf7bda8764d356c1b5e6dd2109ec4
auiechko <auechko@gmail.com>
2026-03-26 23:11:05 +0200

    Disabled csrf for /api/jwt/** endpoints

--- src/main/java/com/example/telos/security/SecurityConfig.java
+++ src/main/java/com/example/telos/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .csrf( csrf -> csrf
-                        .ignoringRequestMatchers("/api/auth/**")
+                        .ignoringRequestMatchers("/api/jwt/**")
                 )
                 .exceptionHandling(ex -> ex
                         .defaultAuthenticationEntryPointFor(
