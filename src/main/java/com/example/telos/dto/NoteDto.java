986fcd6f56553ebbb663db19dc55e492c31fe319
auiechko <auechko@gmail.com>
2026-04-06 14:06:02 +0300

    Added message if text is too big

--- src/main/java/com/example/telos/dto/NoteDto.java
+++ src/main/java/com/example/telos/dto/NoteDto.java
@@ -11,6 +11,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class NoteDto {
     @NotBlank(message = "Note text cannot be empty")
-    @Size(max = 1200)
+    @Size(max = 1200, message = "size the element must be lower or equal to 1200")
     private String noteText;
 }
